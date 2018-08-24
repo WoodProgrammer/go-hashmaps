dff3fc031a0e0c02974d36a9631f89924169ba53
Emir Özbir <emirozbir@hotmail.com>
2018-08-25 01:52:47 +0300

    First Commit

---
+++ linkedList.go
@@ -0,0 +1,47 @@
+package main
+import "fmt"
+type Data struct {
+  value string
+  next *Data
+
+}
+
+
+type List struct {
+	length int
+	start  *Data
+}
+
+
+func (list *List ) setToTheList(data *Data ){
+  if (list.length == 0){
+    list.start = data;
+  }else{
+    exist := list.start;
+    for exist.next != nil { //getter method code :==)))
+      exist = exist.next;
+
+    }
+    exist.next = data
+  }
+  list.length++
+}
+
+func (list *List ) getFromList() {
+
+  for list.start != nil {
+    fmt.Println(list.start.value)
+    list.start = list.start.next;
+  }
+
+}
+
+
+func main(){
+
+  list := &List{}
+  data := Data {
+    value: "Kobrake123"
+  }
+  
+}
